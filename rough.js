e23f0388a354029e4887a3c6bb3c0b505fcba1ce
Prafful kumar <kumarprafful.54@gmail.com>
2018-10-23 03:04:10 +0530

    final

---
+++ rough.js
@@ -0,0 +1,3 @@
+"if (document.referrer == "https://www.spacex.com/mars"){   document.getElementById("root").innerHTML = "UkNURihmM2FyX2chdjNzXyFudDNsbCFnM25jM18zdjNuX3QwX2YwMGxzKQ=="; } else{   document.getElementById("root").innerHTML = "Olny users coming from Mars are allowed"; }"
+
+if (document.referrer == "https://www.spacex.com/mars"){   document.getElementById("root").innerHTML = "UkNURihmM2FyX2chdjNzXyFudDNsbCFnM25jM18zdjNuX3QwX2YwMGxzKQ=="; } else{   document.getElementById("root").innerHTML = "Only users coming from Mars are allowed"; }
